17035627cd639dca888d7d7780e789d991335e81
ALEX <sg014877@gmail.com>
2023-06-05 23:57:42 +0300

    add new ver

--- js/initMap.js
+++ js/initMap.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: 50.616, lng: 26.276 };
+  const uluru = { lat: 50.61664535369396, lng: 26.276761129148703 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 13,
     center: uluru,
   });
   // The marker, positioned at Uluru
