edc87b9bc93228103420e78a343f6e3ad158021b
ALEX <sg014877@gmail.com>
2023-07-01 22:46:41 +0300

    add

--- js/script.js
+++ js/script.js
@@ -78,6 +78,28 @@ funcSlideFooterGallery = function(){
 }
 funcSlideFooterGallery()
 
+// функції взяті з інету перелистування картинок в першому блоці футера
+funcSlideFooterGallery = function(){
+  $('.slider').each(function() {
+    var $gal     = $(this),
+        $movable = $(".movable", $gal), 
+        $slides  = $(">*", $movable),
+        N        = $slides.length,
+        C        = 0,
+        itv      = null;
+    function play() { itv = setInterval(anim, 3000); }
+    function stop() { clearInterval(itv); }
+    function anim() {
+      C = ($(this).is(".prev") ? --C : ++C) <0 ? N-1 : C%N;
+      $movable.css({transform: "translateX(-"+ (C*100) +"%)"});
+    }
+    $(".prev, .next", this).on("click", anim);
+    $gal.hover(stop, play);
+    play();
+  });
+}
+funcSlideFooterGallery()
+
 // функція ховання, показу хедера при скролі
 var st = $(window).scrollTop();
 var navheight = $('.header').height() + 15;
